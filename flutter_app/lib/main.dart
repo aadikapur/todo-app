74bb20cc629940a6ef0d6663fde7b478ea4b5ae7
jchoi630 <john.choi630@gmail.com>
2021-02-07 23:02:24 -0500

    CHanged a lot of the code. Adding the guide I sued to make it ain the readme

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,8 +10,8 @@ void main() async {
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
         brightness: Brightness.light,
-        primaryColor: Colors.blue,
-        accentColor: Colors.orange),
+        primaryColor: Colors.amberAccent[700],
+        accentColor: Colors.greenAccent),
     home: MyApp(),
   ));
 }
@@ -22,25 +23,49 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String todoTitle = "";
+  String selfImportance;
+
+  getTodoTitle(title) {
+    this.todoTitle = title;
+  }
+  getImportance(importance) {
+    this.selfImportance  = importance;
+  }
 
   createTodos() {
     DocumentReference documentReference =
-        FirebaseFirestore.instance.collection("MyTodos").doc(todoTitle);
+    FirebaseFirestore.instance.collection("MyTodos").doc(todoTitle);
 
     //Map
-    Map<String, String> todos = {"todoTitle": todoTitle};
+    Map<String, dynamic> todos = {
+      "todoTitle": todoTitle,
+      "selfImportance":selfImportance};
 
     documentReference.set(todos).whenComplete(() {
       print("$todoTitle created");
     });
   }
 
-  deleteTodos(item) {
+  updateTodos() {
     DocumentReference documentReference =
-        FirebaseFirestore.instance.collection("MyTodos").doc(item);
+    FirebaseFirestore.instance.collection("MyTodos").doc(todoTitle);
+
+    //Map
+    Map<String, dynamic> todos = {
+      "todoTitle": todoTitle,
+      "selfImportance":selfImportance};
+
+    documentReference.set(todos).whenComplete(() {
+      print("$todoTitle updated");
+    });
+  }
+
+  deleteTodos() {
+    DocumentReference documentReference =
+    FirebaseFirestore.instance.collection("MyTodos").doc(todoTitle);
 
     documentReference.delete().whenComplete(() {
-      print("$item deleted");
+      print("$todoTitle deleted");
     });
   }
 
@@ -48,79 +73,105 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("mytodos"),
+        title: Text("List of things to do"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  title: Text("Add Todolist"),
-                  content: TextField(
-                    onChanged: (String value) {
-                      todoTitle = value;
-                    },
-                  ),
-                  actions: <Widget>[
-                    FlatButton(
-                        onPressed: () {
-                          createTodos();
-
-                          Navigator.of(context).pop();
-                        },
-                        child: Text("Add"))
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.only(bottom:  12.0),
+              child: TextFormField(
+                decoration:  InputDecoration(
+                    labelText: "What task do we need to do?",
+                    fillColor: Colors.white70
+                ),
+                onChanged: (String todoTitle) {
+                  getTodoTitle(todoTitle);
+                },
+              ) ,
+            ),
+        Padding(padding: EdgeInsets.only(bottom:  12.0),
+          child: TextFormField(
+          decoration:  InputDecoration(
+          labelText: "How important??",
+          fillColor: Colors.white70
+          ),
+          onChanged: (String selfImportance) {
+            getImportance(selfImportance);
+          },
+          ) ,
+          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                    color: Colors.greenAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    child : Icon (
+                        Icons.add,
+                        color: Colors.teal
+                    ),
+                    onPressed: createTodos),
+                RaisedButton(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder (
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Icon (
+                        Icons.update,
+                        color: Colors.yellow
+                    ),
+                    onPressed: updateTodos),
+                RaisedButton(
+                    color: Colors.red,
+                    shape: RoundedRectangleBorder (
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Icon(
+                        Icons.delete_forever,
+                        color: Colors.white
+                    ),
+                    onPressed: deleteTodos)
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(4.0),
+                child: Row (
+                  children: <Widget>[
+                    Expanded (
+                      child: Text("Task to do"),
+                    ),
+                    Expanded(
+                        child: Text("Importance"))
                   ],
-                );
-              });
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
+                )),
+            StreamBuilder(
+              stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
+              builder: (context, snapshots) {
+                if (snapshots.hasData) {
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshots.data.docs.length,
+                      itemBuilder: (context, index) {
+                        DocumentSnapshot documentSnapshot =
+                        snapshots.data.docs[index];
+                        return Row(
+                          children: <Widget>[
+                            Expanded(child: Text(documentSnapshot["todoTitle"]),
+                            ),
+                            Expanded(child: Text(documentSnapshot["selfImportance"])
+                            )],
+                        );
+                      });
+                } else {
+                  return Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            )
+          ],
         ),
       ),
-      body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
-          builder: (context, snapshots) {
-            if (snapshots.hasData) {
-              return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshots.data.docs.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot documentSnapshot =
-                        snapshots.data.docs[index];
-                    return Dismissible(
-                        onDismissed: (direction) {
-                          deleteTodos(documentSnapshot["todoTitle"]);
-                        },
-                        key: Key(documentSnapshot["todoTitle"]),
-                        child: Card(
-                          elevation: 4,
-                          margin: EdgeInsets.all(8),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          child: ListTile(
-                            title: Text(documentSnapshot["todoTitle"]),
-                            trailing: IconButton(
-                                icon: Icon(
-                                  Icons.delete,
-                                  color: Colors.red,
-                                ),
-                                onPressed: () {
-                                  deleteTodos(documentSnapshot["todoTitle"]);
-                                }),
-                          ),
-                        ));
-                  });
-            } else {
-              return Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
     );
   }
 }
